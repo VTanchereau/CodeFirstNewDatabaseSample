56dc5b8d01e5bf2fecdb096b44ada2a377d66a2e
Tanchereau <victor.tanchereau@gmail.com>
2017-01-30 11:14:05 +0100

    set up de EntitiFramework

--- CodeFirstNewDatabaseSample/Program.cs
+++ CodeFirstNewDatabaseSample/Program.cs
@@ -3,13 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace CodeFirstNewDatabaseSample
 {
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string Nom { get; set; }
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+        public virtual Blog Blog { get; set; }
+        public virtual List<Tag> Tags { get; set; }
+    }
+
+    public class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+
+
     class Program
     {
         static void Main(string[] args)
         {
+            using (BloggingContext db = new BloggingContext())
+            {
+                // Create and save a new Blog 
+                Console.Write("Enter a name for a new Blog: ");
+                string name = Console.ReadLine();
+
+                Blog blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                // Display all Blogs from the database 
+                IOrderedQueryable<Blog> query = from b in db.Blogs orderby b.Name select b;
+
+                Console.WriteLine("All blogs in the database:");
+                foreach (Blog item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
     }
 }
